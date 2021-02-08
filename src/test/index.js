c003433cabcbb84e31e2773504064fb0f5c6d488
Gu-Miao <1450206741@qq.com>
2021-02-08 20:25:17 +0800

    Change test for dev environment.

--- src/test/index.js
+++ src/test/index.js
@@ -1,7 +1,10 @@
-import { showAlert } from '../index.js'
-showAlert({
+import GmAlert from '../index.js'
+
+const a = GmAlert({
   title: 'This is a title',
   body:
     'Some content some content some content some content some content some content some content some content some content some content some content some content some content some content some content some content some content',
-  maskCloseAble: false
-})
+  maskCloseAble: true
+}).show()
+
+window.a = a
