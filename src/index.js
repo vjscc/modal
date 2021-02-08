e5e198f097c7ae1a2fe0af6690d27318bca731d6
Gu-Miao <1450206741@qq.com>
2021-02-08 20:21:39 +0800

    Rewrite API.

--- src/index.js
+++ src/index.js
@@ -1,97 +1,101 @@
-import { isPlainObject, isFunction, fadeIn, fadeOut, $$ } from './utils'
+import { isPlainObject, isFunction, isUndefined, fadeIn, fadeOut } from './utils'
 
-const mask = document.createElement('div')
+let i = 0
 
 function createMask() {
-  mask.setAttribute('class', 'gm-alert-mask')
-  mask.style.display = 'none'
-  mask.innerHTML = `<div class="modal"><div class="title"></div><div class="body"></div><div class="footer"><button data-type="ok">确定</button><button data-type="cancel">取消</button></div></div>`
-  document.body.append(mask)
+  const $mask = document.createElement('div')
+  $mask.setAttribute('class', 'gm-alert-mask')
+  $mask.setAttribute('id', `ga-${i++}`)
+  $mask.style.display = 'none'
+  $mask.innerHTML = `<div data-role="modal"><div data-role="title"></div><div data-role="body"></div><div data-role="footer"><button data-role="ok">确定</button><button data-role="cancel">取消</button></div></div>`
+  document.body.append($mask)
+  return $mask
 }
 
-if (document.body) {
-  createMask()
-} else {
-  window.addEventListener('load', createMask)
-}
-
-// Stop propagation when click modal
-const modal = $$('.modal', mask)
-modal.addEventListener('click', e => e.stopPropagation())
-
-let currentOnOK
-let currentOnCancel
-
-const showAlert = config => {
+const GmAlert = config => {
   // The config must be a plain object
   if (!isPlainObject(config)) {
-    throw new ReferenceError('config is not plain object')
+    throw new ReferenceError('Config is not plain object.')
   }
 
-  // destruct config
+  // Destruct config
   const {
     title = '',
     titleClassName = '',
     body = '',
     bodyClassName = '',
+    showFooter = true,
     footerClassName = '',
     okText = 'ok',
-    onOK = hideAlert,
+    onOK = undefined,
     cancelText = 'cancel',
-    onCancel = hideAlert,
-    maskClosAble = true
+    onCancel = undefined,
+    maskClosAble = true,
+    isShow = false
   } = config
 
-  // validate config item
-  if (!isFunction(onOK)) {
-    throw new ReferenceError('onOK is not a function')
+  // Validate config item
+  if (!isFunction(onOK) && isUndefined(onOK)) {
+    throw new ReferenceError(`Config item 'onOK' is not a function or undefined.`)
   }
-  if (!isFunction(onCancel)) {
-    throw new ReferenceError('onCancel is not a function')
+  if (!isFunction(onCancel) && isUndefined(onCancel)) {
+    throw new ReferenceError(`Config item 'onCancel' is not a function or undefined.`)
   }
+
+  const instance = { ...config }
+
+  instance.__proto__ = GmAlert.prototype
+
+  instance.$mask = createMask()
+  maskClosAble && instance.$mask.addEventListener('click', () => instance.hide())
+
+  instance.$modal = instance.$mask.querySelector('[data-role="modal"]')
+  instance.$modal.addEventListener('click', e => e.stopPropagation())
 
   // Some elements
-  const _title = $$('.title', modal)
-  const _body = $$('.body', modal)
-  const footer = $$('.footer', modal)
-  const okBtn = $$('[data-type="ok"]', footer)
-  const cancelBtn = $$('[data-type="cancel"]', footer)
+  instance.$title = instance.$modal.querySelector('[data-role="title"]')
+  instance.$body = instance.$modal.querySelector('[data-role="body"]')
+  instance.$footer = instance.$modal.querySelector('[data-role="footer"]')
+  instance.$ok = instance.$footer.querySelector('[data-role="ok"]')
+  instance.$cancel = instance.$footer.querySelector('[data-role="cancel"]')
 
   // Set title
-  _title.innerHTML = title
-  _title.style.display = title ? 'block' : 'none'
-  _title.setAttribute('class', `title ${titleClassName}`)
+  instance.$title.innerHTML = title
+  instance.$title.style.display = title ? 'block' : 'none'
+  titleClassName && instance.$title.setAttribute('class', titleClassName)
 
   // Set body
-  _body.innerHTML = body
-  _body.setAttribute('class', `body ${bodyClassName}`)
+  instance.$body.innerHTML = body
+  bodyClassName && instance.$body.setAttribute('class', bodyClassName)
 
   // Set footer
-  footer.setAttribute('class', `footer ${footerClassName}`)
+  footerClassName && instance.$footer.setAttribute('class', footerClassName)
+  if (!showFooter) instance.$footer.style.display = 'none'
 
   // Set OK button
-  okBtn.innerHTML = okText
-  okBtn.removeEventListener('click', currentOnOK)
-  okBtn.addEventListener('click', (currentOnOK = onOK))
+  instance.$ok.innerHTML = okText
+  instance.$ok.addEventListener('click', onOK ?? (() => instance.hide()))
 
   // Set cancel button
-  if (cancelText) {
-    cancelBtn.style.display = 'inline-block'
-    cancelBtn.innerHTML = cancelText
-    cancelBtn.removeEventListener('click', currentOnCancel)
-    cancelBtn.addEventListener('click', (currentOnCancel = onCancel))
-  } else {
-    cancelBtn.style.display = 'none'
-  }
+  instance.$cancel.innerHTML = cancelText
+  instance.$cancel.addEventListener('click', onCancel ?? (() => instance.hide()))
 
-  // Set mask of modal
-  mask.removeEventListener('click', currentOnCancel)
-  if (maskClosAble) {
-    mask.addEventListener('click', (currentOnCancel = onCancel))
-  }
-  fadeIn(mask, 'flex')
+  // Show modal or not
+  isShow && instance.show()
+
+  return instance
 }
 
-const hideAlert = () => fadeOut(mask)
+GmAlert.prototype.show = function () {
+  fadeIn(this.$mask, 'flex')
+  this.isShow = true
+  return this
+}
 
-export { showAlert, hideAlert }
+GmAlert.prototype.hide = function () {
+  fadeOut(this.$mask)
+  this.isShow = false
+  return this
+}
+
+export default GmAlert
