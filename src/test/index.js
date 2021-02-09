683171b7fbffd98116be9432299b9f4ef22a27bb
Gu-Miao <1450206741@qq.com>
2021-02-09 18:08:40 +0800

    Add more of test.

--- src/test/index.js
+++ src/test/index.js
@@ -1,10 +1,49 @@
 import GmAlert from '../index.js'
 
-const a = GmAlert({
-  title: 'This is a title',
-  body:
-    'Some content some content some content some content some content some content some content some content some content some content some content some content some content some content some content some content some content',
-  maskCloseAble: true
-}).show()
+let header = ''
+let headerClass = ''
+let body = ''
+let bodyClass = ''
+let footerClass = ''
+let okText = 'ok'
+let cancelText = 'cancel'
 
-window.a = a
+const alert = GmAlert({
+  header,
+  headerClass,
+  body,
+  bodyClass,
+  footerClass,
+  okText,
+  cancelText,
+  maskCloseAble: true,
+  isShow: false
+})
+
+const $form = document.querySelector('form')
+const $header = $form.querySelector('#header')
+const $headerClass = $form.querySelector('#header-class')
+const $body = $form.querySelector('#body')
+const $bodyClass = $form.querySelector('#body-class')
+const $footerClass = $form.querySelector('#footer-class')
+const $ok = $form.querySelector('#ok')
+const $cancel = $form.querySelector('#cancel')
+
+$form.addEventListener('submit', e => {
+  e.preventDefault()
+  header = $header.value
+  headerClass = $headerClass.value
+  body = $body.value
+  bodyClass = $bodyClass.value
+  footerClass = $footerClass.value
+  okText = $ok.value
+  cancelText = $cancel.value
+
+  alert
+    .setHeader(header, headerClass)
+    .setBody(body, bodyClass)
+    .setFooter(undefined, true, footerClass)
+    .setOK(okText)
+    .setCancel(cancelText)
+    .show()
+})
