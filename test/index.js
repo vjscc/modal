726d509e868f580f6e9ebf7c061e03b8c837b374
Gu-Miao <1450206741@qq.com>
2021-10-31 21:11:13 +0800

    Update test page.

--- test/index.js
+++ test/index.js
@@ -1,3 +1,9 @@
+window.VjsccModal.config({
+  isShow: true,
+  maskClose: false,
+  maskColor: '#32abf1'
+})
+
 const modal = new window.VjsccModal({
   $mask: '.vjscc-modal-mask',
   onOK: function (e) {
