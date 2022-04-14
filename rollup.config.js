93d950f85a7d88e1070df1280091458ceac67a23
Gu-Miao <1450206741@qq.com>
2022-04-14 14:07:49 +0800

    Update config
    - Upgrade `rollup-plugin-ts` and use `swc` as compiler
    - Update rollup plugins and config
    - Remove `parcel`
    - Remove removeRedundancyDeclarations.js
    - Update scripts
    - Add `@vjscc/utils` as `external`
    - Add bundled build for browser

--- rollup.config.js
+++ rollup.config.js
@@ -2,46 +2,124 @@ import path from 'path'
 import pkg from './package.json'
 
 import clear from 'rollup-plugin-clear'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import less from 'rollup-plugin-less'
 import ts from 'rollup-plugin-ts'
 import { terser } from 'rollup-plugin-terser'
+import LessPluginCleanCSS from 'less-plugin-clean-css'
 
-/** @type import('rollup').RollupOptions */
-const config = {
-  input: path.join(__dirname, 'src/index.ts'),
-  output: [
-    {
-      name: pkg.global,
-      file: pkg.main,
-      format: 'umd',
-      exports: 'auto'
-    },
-    {
-      name: pkg.global,
-      file: pkg.browser,
-      format: 'iife',
-      exports: 'auto',
-      plugins: [terser()],
-      sourcemap: true
-    },
-    {
-      file: pkg.module,
-      format: 'esm',
-      exports: 'auto'
-    }
-  ],
+const isProduction = process.env.NODE_ENV === 'production'
+const input = path.join(__dirname, 'src/index.ts')
+const name = 'VjsccModal'
 
-  plugins: [
+function getPlugins(lessConfig = {}, declaration) {
+  return [
     clear({
       targets: ['dist']
     }),
     resolve(),
     commonjs(),
+    less({ output: false, ...lessConfig }),
     ts({
-      transpiler: 'babel'
+      transpiler: 'swc',
+      tsconfig: {
+        declaration
+      }
     })
   ]
 }
+
+/**
+ * @typedef RollupOptions
+ * @type {import('rollup').RollupOptions}
+ */
+
+/**
+ * @returns {RollupOptions}
+ */
+function createDevConfig() {
+  return {
+    input,
+    output: {
+      name,
+      file: pkg.main,
+      format: 'umd',
+      exports: 'auto',
+      sourcemap: true
+    },
+    plugins: getPlugins({ insert: true }, true)
+  }
+}
+
+/**
+ * @returns {RollupOptions[]}
+ */
+function createProdConfig() {
+  return [
+    {
+      input,
+      output: {
+        name,
+        file: pkg.main,
+        format: 'umd',
+        exports: 'auto',
+        sourcemap: true,
+        globals: {
+          '@vjscc/utils': 'VjsccUtils'
+        }
+      },
+      plugins: getPlugins({ output: 'dist/index.css' }, true),
+      external: '@vjscc/utils'
+    },
+    {
+      input,
+      output: {
+        file: pkg.module,
+        format: 'esm',
+        exports: 'auto',
+        sourcemap: true,
+        globals: {
+          '@vjscc/utils': 'VjsccUtils'
+        }
+      },
+      plugins: getPlugins(),
+      external: '@vjscc/utils'
+    },
+    {
+      input,
+      output: {
+        name,
+        file: 'dist/browser/vjscc-modal.min.js',
+        format: 'umd',
+        exports: 'auto',
+        sourcemap: true,
+        plugins: [terser()],
+        globals: {
+          '@vjscc/utils': 'VjsccUtils'
+        }
+      },
+      plugins: getPlugins({
+        output: 'dist/browser/vjscc-modal.min.css',
+        plugins: [new LessPluginCleanCSS({ advanced: true })]
+      }),
+      external: '@vjscc/utils'
+    },
+    {
+      input,
+      output: {
+        name,
+        file: 'dist/browser/vjscc-modal.bundle.min.js',
+        format: 'umd',
+        exports: 'auto',
+        sourcemap: true,
+        plugins: [terser()]
+      },
+      plugins: getPlugins()
+    }
+  ]
+}
+
+const config = isProduction ? createProdConfig() : createDevConfig()
 
 export default config
--- test/index.js
+++ test/index.js
@@ -1,14 +1,18 @@
-window.VjsccModal.config({
+/** @type {typeof import('..').default} */
+const VjsccModal = window.VjsccModal
+
+VjsccModal.config({
   isShow: true,
   maskClose: false,
   maskColor: '#32abf1'
 })
 
-const modal = new window.VjsccModal({
+const modal = new VjsccModal({
+  duration: 1000,
   $mask: '.vjscc-modal-mask',
   onOK: function (e) {
     console.log(e, this)
   }
 })
 
-window.m = modal
+console.log(modal)
